150b82129443127f53c0a84f89646e46eeb15481
Cameron Schuette <cschuette@criticalmix.com>
2016-08-12 13:14:12 -0400

    responsive popup library

---
+++ responsive-popup.js
@@ -0,0 +1,43 @@
+function ResponsivePopup () {
+    this.popup = null;
+}
+
+ResponsivePopup.prototype.open = function (obj) {
+    /** Required Params */
+    if (obj.content == null) {
+        console.error("No body content included");
+        return;
+    } else if (!$(obj.content).length) {
+        console.error("No body content included");
+        return;
+    }
+
+    /** Default Values */
+    if (obj.closeBtn == null) {
+        obj.closeBtn = true;
+    }
+
+    $(obj.content).removeClass('rsp-hide');
+    $(obj.content).addClass('content');
+    this.popup = $(obj.content).wrap("<div class='rsp'></div>");
+
+    if (obj.closeBtn) {
+        this.popup.prepend("<div class='close-button' onclick='rsp.close();'><i class='fa fa-times' aria-hidden='true'></i></div>");
+    }
+
+    $("body").append(
+        $("<div>", {
+                "class" : "rsp-overlay",
+                "onclick" : "rsp.close()"
+            }
+        ));
+};
+
+ResponsivePopup.prototype.close = function() {
+    $('.rsp .close-button').remove();
+    this.popup.unwrap();
+
+    this.popup.addClass('rsp-hide');
+
+    $('.rsp-overlay').remove();
+};
